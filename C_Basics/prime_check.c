9567811d667747b60b00e343fbcbdf74ff8454f3
nairkartik08 <nairkartik08@gmail.com>
2025-07-10 19:06:50 +0530

    Create prime_check.c

---
+++ C_Basics/prime_check.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+int main() 
+{
+    int n,i=2;
+    printf("Enter a number: ");
+    scanf("%d",&n);
+    while (n%i!=0)
+    {
+        i++;
+    }
+    if (i==n)
+    printf("%d is a prime number",n);
+    else
+    printf("%d is not a prime number",n);
+    return 0;
+}
